59296a5c0f82f6e43e711e6e374ffee2f8553ce5
yuricampolongo <yuricampolongo@outlook.com>
2018-07-23 18:48:11 -0300

    Practicing JSX expressions

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,7 +1,35 @@
 console.log('App.js is running...');
 
+var app = {
+    title:'Indecision app',
+    subtitle:'React project'
+};
+
 //JSX - JavaScript XML
-var template = <h1>Indecision App</h1>;
+var template = (
+    <div>
+        <h1>{app.title}</h1>
+        <p>{app.subtitle}</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div>
+);
+
+var user = {
+    name:'Walter Geoffrey',
+    age: 2,
+    location: 'USA'
+};
+var templateTwo = (
+    <div>
+        <h1>{user.name}</h1>
+        <p>Age: {user.age}</p>
+        <p>Location: {user.location}</p>
+    </div>
+);
+
 var root = document.getElementById('app');
 
 ReactDOM.render(template,root);
