9d63dec8e4bbab2a6c076ae8e6111f4e57e91862
Yuri Campolongo <yuri.campolongo@voxage.com.br>
2018-11-28 17:49:27 -0200

    Studying react states

---
+++ indecision-app/src/playground/counter-example.js
@@ -0,0 +1,50 @@
+class Counter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleAddOne = this.handleAddOne.bind(this);
+        this.handleMinusOne = this.handleMinusOne.bind(this);
+        this.handleReset = this.handleReset.bind(this);
+        this.state = {
+            count: 0
+        };
+    }
+
+    handleAddOne() {
+        this.setState((prevState) => {
+            return {
+                count: prevState.count + 1
+            }
+        });
+    }
+
+    handleMinusOne() {
+        this.setState((prevState) => {
+            return {
+                count: prevState.count - 1
+            }
+        });
+    }
+
+    handleReset() {
+        this.setState(() => {
+            return {
+                count: 0
+            }
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Count: {this.state.count}</h1>
+                <button onClick={this.handleAddOne}>+</button>
+                <button onClick={this.handleMinusOne}>-</button>
+                <button onClick={this.handleReset}>reset</button>
+            </div>
+        );
+    }
+}
+
+
+
+ReactDOM.render(<Counter />, document.getElementById('app'));
